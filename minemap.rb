affe4e40a55a325237c829bd7ac5d58a25a2def6
sStellar <rexstellar.pa@gmail.com>
2020-04-16 14:13:45 +0200

    Fixed points issue

--- minemap.rb
+++ minemap.rb
@@ -1,3 +1,5 @@
+require "colorize"
+
 map_width = 10 # map will be map_w^2 in size
 mines = 10
 map = []
@@ -11,19 +13,27 @@ map_width.times do
   map << temp_arr
 end
 
-i3 = 0
+index = 0
 mines.times do
   temp_coords = [rand(0...map_width),rand(0...map_width)]
   mines_coords << temp_coords
-  print "#{mines_coords[i3]}\n"
-  i3 += 1
+  print "#{mines_coords[index]}\n"
+  index += 1
 end
 
-print mines_coords
-print "\n"
 i = 0
 while i < mines
-  map[mines_coords[i][0]][mines_coords[i][1]] = "X"
-  print "#{map[i]}\n"
+  map[(mines_coords[i][0])][(mines_coords[i][1])] = "X"
   i += 1
+end
+
+map.each do |row|
+  row.each do |point|
+    if point == "X"
+      print "#{point.colorize(:red)} "
+    else
+      print "#{point.colorize(:light_blue)} "
+    end
+  end
+  print "\n"
 end
